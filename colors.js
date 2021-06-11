8a054f65ae2aa6ecbe06d72c0352b9764fe3cd8d
Marshall-Wilson <72046534+Marshall-Wilson@users.noreply.github.com>
2021-06-10 23:07:09 -0500

    Color picker v2 with gradient animation

--- colors.js
+++ colors.js
@@ -1,27 +1,75 @@
 const frame = () => {
+    if (running) {
+        for (let i = 0; i < stops.length - 1; i++) {
+            stops[i] = stops[i + 1];
+        }
+        frameRule++;
+        if (frameRule === changeFrequency) {
+            frameRule = 0;
+            stops[stops.length - 1] = incrementColor(stops[stops.length - 2]);
+        } else {
+            stops[stops.length - 1] = stops[stops.length - 2];
+        }
+        document.getElementById("background").style.backgroundImage =
+            makeGradient();
+        document.getElementById("swatchColor").style.backgroundColor =
+            `rgb(${stops[0][0]}, ${stops[0][1]}, ${stops[0][2]})`;
+        document.getElementById("rVal").innerHTML = stops[0][0];
+        document.getElementById("gVal").innerHTML = stops[0][1];
+        document.getElementById("bVal").innerHTML = stops[0][2];
+    } else {
+        clearInterval(id);
+    }
+}
+
+const incrementColor = input => {
+    let output = [0, 0, 0];
     for (let i = 0; i < 3; i++) {
-        rgbCols[i] += rgbIncs[i];
-        if (rgbCols[i] < 200 || rgbCols[i] > 250) {
+        output[i] = input[i] + rgbIncs[i]
+        if (output[i] > rgbMax || output[i] < rgbMin) {
             rgbIncs[i] *= -1;
         }
     }
-    document.getElementById("background").style.backgroundColor =
-        `rgb(${rgbCols[0]}, ${rgbCols[1]}, ${rgbCols[2]})`;
-    document.getElementById("rVal").innerHTML = rgbCols[0];
-    document.getElementById("gVal").innerHTML = rgbCols[1];
-    document.getElementById("bVal").innerHTML = rgbCols[2];
+    return output;
 }
-
 
 const runAnimation = () => {
     let bg = document.getElementById("background").style;
-    bg.backgroundColor = `rgb(${rgbCols[0]}, ${rgbCols[1]}, ${rgbCols[2]})`;
+    bg.backgroundImage = makeGradient();
     let id = null;
     clearInterval(id);
-    id = setInterval(frame, 200);
+    id = setInterval(frame, frameRate);
 }
 
-let rgbCols = [200, 200, 200];
-let rgbIncs = [1, 2, 3];
+const makeGradient = () => {
+    let gradient = "linear-gradient(to bottom right ";
+    for (let i = 0; i < stops.length; i++) {
+        gradient += ", ";
+        gradient += `rgb(${stops[i][0]}, ${stops[i][1]}, ${stops[i][2]})`;
+    }
+    gradient += ")";
+    return gradient;
+}
 
-document.getElementById("background").addEventListener("click", runAnimation);
+const pausePlay = () => {
+    running = !running;
+}
+
+
+let rgbIncs = [3, 4, 5];
+let running = true;
+let frameRule = 0;
+
+const changeFrequency = 1;
+const stopNumber = 10;
+const rgbMax = 250; //rgbMax + any rgbInc value cannot exeed 255
+const rgbMin = 50;
+const frameRate = 75;
+
+
+let stops = [];
+for (let i = 0; i < stopNumber; i++) {
+    stops.push([rgbMin, rgbMax, rgbMin]);
+}
+document.getElementById("background").addEventListener("click", pausePlay);
+runAnimation();
