829464cd56986df111e8ff488fa5e718cf141f90
Marshall-Wilson <72046534+Marshall-Wilson@users.noreply.github.com>
2021-11-14 16:32:13 -0500

    added redirect to home page from artist explorer

--- src/components/explorer/Nav.jsx
+++ src/components/explorer/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 //explorer nav bar
 const Nav = ({clickHandler}) => {
     return (
         <div className="nav">
-            <h1>Spotify Artist Explorer</h1>
+            <Link to="/"><h1>Spotify Artist Explorer</h1></Link>
             <div className="nav-links">
                 <h2 onClick={() => clickHandler('main')}>Main</h2>
                 <h2 onClick={() => clickHandler('about')}>About</h2>
